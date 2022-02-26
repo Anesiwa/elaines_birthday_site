b940a3f700fe280b2c9dcca4a5b5993ea96b6660
Anesiwa <89821512+Anesiwa@users.noreply.github.com>
2022-02-25 21:25:02 -0600

    Created Elaine's birthday app in react.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Elaine's Sassy Sixty</header>
+      <div>Hello</div>
+    </div>
+  );
+}
+
+export default App;
